c4d59f27d70cac2ca01578109c1beaf5ea99a2c6
GhostVeyron410 <46247418+GhostVeyron410@users.noreply.github.com>
2018-12-29 18:22:57 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log("Bot Ready !");
+});
+
+bot.login('NTI2MzU1MTkwMjY2NDYyMjA4.DwEAIg.zdY0wHUxNpno8soF9PZ5NZHUbnI');
